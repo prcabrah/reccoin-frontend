2620bd3d0fd16a872d44f5e4a73f8d66d3cdc372
Inioluwa Adewara <adewarainioluwa@gmail.com>
2023-05-10 05:21:51 -0700

    Scaffold created

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+
+    <div class='max-w-[32rem] mt-2rem mx-auto'>
+      <p>This is the Home Page to the <span class="text-blue-800">ReCoin Project</span>. 
+        Please note that the site is currently undergoing development.
+        Thanks.
+      </p>
+    </div>
+  )
+}
+
+export default App
