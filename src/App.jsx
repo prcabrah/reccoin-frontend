2083ce54bd4a4563fa2acc87ee982a19eda19070
Peculiar Richard <pecullozie@gmail.com>
2023-05-16 14:41:33 +0100

    created the mobile responsive hero section of the landing page

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Hero from './components/homepage_components/Hero'
 
 function App() {
 
   return (
 
-    <div class='max-w-[32rem] mt-2rem mx-auto'>
-      <p>This is the Home Page to the <span class="text-blue-800">ReCoin Project</span>. 
+    <>
+      <Hero></Hero>
+      {/* <p>This is the Home Page to the <span class="text-blue-800">ReCoin Project</span>. 
         Please note that the site is currently undergoing development.
         Thanks.
-      </p>
-    </div>
+      </p> */}
+    </>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'herobg': "url('/src/assets/hero-bg.png')"
+      }
+    },
   },
   plugins: [],
 }
