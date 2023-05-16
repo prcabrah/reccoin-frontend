b44b6c3a6b4e43d2d8a526fb77a05dcf9c3c4f05
Absolute <106808580+chiemezie1@users.noreply.github.com>
2023-05-16 15:48:27 +0100

    header component added

---
+++ src/components/navigation/header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="header bg-white h-100 w-full sm:w-1503 absolute shadow-md">
+      
+    </div>
+  
+    )
+}
+
+export default Header
