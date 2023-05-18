6a44cf09ec9ee516a103fce82cd7664f83875959
iamakeem <bolarinwa_akeem@yahoo.com>
2023-05-19 00:40:59 +0100

    almost done

--- src/pages/CompanyRegPage.jsx
+++ src/pages/CompanyRegPage.jsx
@@ -6,12 +6,12 @@ import Logo from './../components/logo';
 const CompanyRegPage = () => {
   return (
     <div className='w-[34rem] md:w-[62rem] lg:w-[82rem] flex flex-row justify-center'>
-      <div className='w-[32rem] md:w-[60rem] lg:w-[70rem] min-h-[64rem] border border-primary40-600'>
-        <p className='text-[1rem] md:text-[2rem] lg:text-[4.7rem] font-bold text-center'>
+      <div >
+        <p className='text-[1rem] md:text-[2rem] lg:text-[3.7rem] font-bold text-center'>
           Company Registration Page
         </p>
-        <div className='flex flex-row justify-center mt-6'>
-          <div className='w-[10rem] md:w-[22rem] lg:w-[33rem] min-h-[50rem] bg-[#F8F9FB] border border-primary40-700'>
+        <div className='flex flex-row justify-center mt-4'>
+          <div className='w-[10rem] md:w-[22rem] lg:w-[29rem] min-h-[34rem] bg-[#F8F9FB] border border-primary40-700'>
             <div className='flex flex-row mt-10 ml-10'>
               <div className='w-46 h-46 items-center '>
                 <Logo fill='#0D4D00' w='46' h='46' />
@@ -21,7 +21,7 @@ const CompanyRegPage = () => {
               </p>
             </div>
           </div>
-          <div className='w-[10rem] md:w-[22rem] lg:w-[31rem] min-h-[50rem] bg-[url(./assets/company-bg.svg)] bg-contain md:bg-cover bg-right bg-no-repeat'></div>
+          <div className='w-[10rem] md:w-[16rem] lg:w-[21rem] min-h-[34rem] bg-[url(./assets/company-bg.svg)]  bg-contain md:bg-cover bg-right bg-no-repeat'></div>
         </div>
       </div>
     </div>
