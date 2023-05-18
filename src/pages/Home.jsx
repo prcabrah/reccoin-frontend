6d5fd736736c3e7f1e3c516d1d56db68f362fe77
Segun Abraham <abrahametomu@yahoo.com>
2023-05-18 23:59:31 +0000

    earn reccoin section completer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Hero from '../components/homepage_components/hero'
+import EarnReccoin from '../components/homepage_components/earnReccoin'
+import {EarnReccoinReward} from '../components/homepage_components/earnReccoin'
 import LatestNews from "../components/homepage_components/LatestNews.jsx";
 
 const Home = () => {
   return <>
     <Hero />
+    <EarnReccoin />
+    {/* <EarnReccoinReward /> */}
     <LatestNews />
   </>
 }
