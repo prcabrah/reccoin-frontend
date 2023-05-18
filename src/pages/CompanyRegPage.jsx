f222d18d6c94f9f93c3e49f11cc3762a6b34dc07
iamakeem <bolarinwa_akeem@yahoo.com>
2023-05-18 17:52:58 +0100

    companyPage in progress

---
+++ src/pages/CompanyRegPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const CompanyRegPage = () => {
+  return (
+    <div class='w-[34rem] md:w-[62rem] lg:w-[82rem] flex flex-row justify-center'>
+      <div class='w-[32rem] md:w-[60rem] lg:w-[70rem] min-h-[64rem] border border-primary40-600'>
+        <p class='text-[1rem] md:text-[2rem] lg:text-[4rem] font-bold text-center'>
+          Company Registration Page
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default CompanyRegPage;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Hero from '../components/homepage_components/hero'
 import LatestNews from "../components/homepage_components/LatestNews.jsx";
+import CompanyRegPage from './CompanyRegPage';
+
 
 const Home = () => {
   return <>
     <Hero />
     <LatestNews />
+    <CompanyRegPage />
   </>
 }
 
