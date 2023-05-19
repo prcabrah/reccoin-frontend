58bc7fdfda69046fc77ca478d04c794e2cbdfdad
Inioluwa <adewarainioluwa@gmail.com>
2023-05-19 13:16:58 +0100

    Update WhitePaper.jsx

--- src/pages/WhitePaper.jsx
+++ src/pages/WhitePaper.jsx
@@ -1,5 +1,23 @@
 export const WhitePaper = () => {
   return <>
-      <h2>White Paper</h2>
+      <div >
+        <div className="flex items-center justify-start h-[24rem] md:h-[48rem] w-[90%] m-auto">
+        <div class="h-[17rem] md:h-[34rem] w-[70%] border-2 border-primary40 ml-[15%] mt-[2%] rounded">
+     
+        <h1 class="ml-[42%] mt-[2%] text-primary40 font-bold">WHITE PAPER</h1>
+  
+  Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo 
+
+Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo 
+
+Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo 
+
+
+</div>
+</div>
+</div>
   </>
 }
