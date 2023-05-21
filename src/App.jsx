c026cd57c5ff335030f72f1a0cc93665d85a7096
iamakeem <bolarinwa_akeem@yahoo.com>
2023-05-21 09:01:03 +0100

    don't create PR yet, almost done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // import { useState } from 'react'
 import Home from './pages/Home'
-// import Header from './components/navigation/Header.jsx'
+import Header from './components/navigation/Header.jsx'
 import { Route, Routes } from 'react-router-dom'
 import Footer from './components/footer'
 import {WhitePaper} from "./pages/WhitePaper.jsx";
@@ -10,7 +10,7 @@ function App() {
 
   return (
     <>
-      {/* <Header/> */}
+      <Header/>
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/prc" element={''}/>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Hero from '../components/homepage_components/hero'
 import LatestNews from "../components/homepage_components/LatestNews.jsx";
 import CompanyRegPage from './CompanyRegPage';
+import Layout from './../components/Layout';
 
 
 const Home = () => {
   return <>
     <Hero />
     <LatestNews />
+    <Layout />
     <CompanyRegPage />
   </>
 }
