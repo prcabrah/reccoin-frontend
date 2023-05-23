5749dda0e13393648b37d538cc2f3462216171d4
iamakeem <bolarinwa_akeem@yahoo.com>
2023-05-23 01:51:44 +0100

    deposit component done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,18 @@
 // import React from 'react'
-import Hero from '../components/homepage_components/hero'
-import LatestNews from "../components/homepage_components/LatestNews.jsx";
+import Hero from '../components/homepage_components/hero';
+import LatestNews from '../components/homepage_components/LatestNews.jsx';
 import CompanyRegPage from './CompanyRegPage';
 // import Layout from './../components/Layout';
 
 
 const Home = () => {
-  return <>
-    <Hero />
-    <LatestNews />
-    <CompanyRegPage />
-  </>
-}
+  return (
+    <>
+      <Hero />
+      <LatestNews />
+      <CompanyRegPage />
+    </>
+  );
+};
 
-export default Home
+export default Home;
