c3a3318f6f7a9111b17f2780cbc236678aa8239c
davebeloed <idadakpoyedavidson@gmail.com>
2023-05-25 11:49:42 +0100

    added the impact of plastic recycling in our environment page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import { EarnReccoinReward } from '../components/homepage_components/earnReccoin
 import LatestNews from '../components/homepage_components/LatestNews.jsx';
 import About from '../components/homepage_components/about';
 import Faq from '../components/homepage_components/faq';
+
 // import CompanyRegPage from './CompanyRegPage';
 
 const Home = () => {
