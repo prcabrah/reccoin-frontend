89263c9ca90df21921a9817ff900864b7e66ef7d
Inioluwa Adewara <adewarainioluwa@gmail.com>
2023-06-01 15:51:33 +0100

    about us corrected

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,12 +1,26 @@
 // import React from 'react';
 
 import AboutUsHero from "../components/about_us_components/AboutUsHero";
+import Empowering from "../components/about_us_components/Empowering";
+import JoinUs from "../components/about_us_components/JoinUs";
+import Values from "../components/about_us_components/Values";
+import Vision from "../components/about_us_components/Vision";
+import Footer from "../components/footer";
+import HomeFooter from "../components/homepage_components/HomeFooter";
 
 const AboutUs = () => {
+  return(
     <>
       <AboutUsHero/>
-      <AboutUs
+      <Values/>
+      <Vision/>
+      <Empowering/>
+      <JoinUs/>
+      <HomeFooter/>
+      <Footer/>
     </>
+  )
+   
     
      };
 
