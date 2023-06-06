841798ae7cdab8d4c994b960fa2173b55fef5dc3
Peculiar Richard <pecullozie@gmail.com>
2023-06-06 22:52:03 +0100

    add:mobile responsiveness for homepage HeroSection

--- src/components/buttons/mediumButton.jsx
+++ src/components/buttons/mediumButton.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const MediumButton = ({text, linkto}) => {
   return (
-    <Link to={`${linkto}`} className='w-32 bg-primary40 p-4 text-white rounded-[8px] hover:tracking-wide ease-in-out duration-300'>{text}</Link>
+    <Link to={`${linkto}`} className=' text-sm w-32 bg-primary40 p-4 mt-6 text-white rounded-[8px] hover:tracking-wide ease-in-out duration-300'>{text}</Link>
   )
 }
 
--- src/components/homepage_components/HeroSection.jsx
+++ src/components/homepage_components/HeroSection.jsx
@@ -5,13 +5,13 @@ import MediumButton from "../buttons/mediumButton";
 const Hero = () => {
   return (
     <>
-      <section className=" border-t-2 mt-20 bg-white">
-        <div className="mt-20 pb-20 mx-auto bg-herobg bg-contain bg-no-repeat  bg-right ">
-          <div className=" flex w-[90%] mx-auto justify-start items-start">
-            <div className="flex flex-col w-[45%]">
-              <h2 className="text-4xl font-bold">RECYCLING</h2>
-              <h2 className="text-6xl tracking-wide font-bold text-[#006D44]">WASTE</h2>
-              <p className="text-[25px] tracking-tight leading-normal mt-5 font-bold">
+      <section className="h-full border-t-2 mt-20 bg-white">
+        <div className="mt-10 md:mt-20 pb-20 mx-auto bg-herobg lg:bg-contain bg-no-repeat sm:bg-right">
+          <div className=" flex flex-col lg:flex-row w-[90%] mx-auto justify-start items-center">
+            <div className="flex flex-col lg:w-[60%] xl:[45%] text-center items-center lg:items-start lg:text-left">
+              <h2 className=" text-3xl md:text-4xl font-bold">RECYCLING</h2>
+              <h2 className=" text-4xl md:text-6xl tracking-wide font-bold text-[#006D44]">WASTE</h2>
+              <p className="text-[18px] md:text-[25px] tracking-tight leading-normal mt-5 font-bold">
                 Revolutionizing Waste Management with Rewards! Turn your Waste
                 into Wealth and Make a Positive Impact on the Planet.
               </p>
@@ -19,14 +19,14 @@ const Hero = () => {
                 <a href="#">Learn More</a>
               </button> */}
                {/* learn more button */}
-             <MediumButton text={"Learn more"} linkto={'/about-us'}/>
+             <MediumButton text={"Learn more"} linkto={'/about-us'} className=""/>
             </div>
 
             <div>
               <img
                 src={heroImage}
                 alt="home-img"
-                className="-mt-28"
+                className="lg:-mt-28 mt-2"
               />
             </div>
           </div>
